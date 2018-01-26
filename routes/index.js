7fcf7791016afbfc1d6af5c1071680682f4b8cb4
DMonMac <dmmacaso@gmail.com>
2018-01-26 16:03:36 +0800

    Able to upload a markdown file and output it in html

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
 //Dependencies
-var express = require('express');
-var router = express.Router();
-var marked = require('marked');
-//var markdownToHTML = require('markdown-to-html');
+const express = require('express');
+const router = express.Router();
+const marked = require('marked');
+const fs = require('fs');
+const path = require('path');
+const multer  = require('multer');
+
+
+
+var Markdown = require('markdown-to-html').Markdown;
+
+const upload = multer({
+  dest: path.join(__dirname, '../public/mdFiles/temp')
+});
+
 //Controllers
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +22,19 @@ router.get('/', function(req, res, next) {
     title: 'MD to HTML Package Tester',
     output: 'This will output the converted MD with the selected package'
   });
+});
+
+router.get('/mdfile/upload', (req, res) => {
+  let path = __dirname + '/../public/mdFiles/README.md';
+  let file = fs.readFileSync(path, 'utf8');
+  res.send("This is a sample output: " + marked(file.toString()));
+});
+
+router.post('/mdfile/upload', upload.single('markdown'), (req, res) => {
+  console.log(req.file)
+  let fileLoc = req.file.path;
+  let fileToRender = fs.readFileSync(fileLoc, 'utf8');
+  res.send(marked(fileToRender.toString()));
 });
 
 router.post('/marked', function(req, res){
