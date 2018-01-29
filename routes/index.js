c67950432e86dee0a86e5a89eb792298a3d28cee
DMonMac <dmmacaso@gmail.com>
2018-01-29 08:46:25 +0800

    Now able to render HTML of MD files

--- routes/index.js
+++ routes/index.js
@@ -6,36 +6,60 @@ const fs = require('fs');
 const path = require('path');
 const multer  = require('multer');
 
+// Use dependencies
 const upload = multer({
-  dest: path.join(__dirname, '../public/mdFiles/temp')
+  dest: path.join(__dirname, '../uploads/mdFiles/temp/')
 });
 
-//Controllers
+// Imported Controllers
+
+// Repeated functions
+function deleteLastUpload() { // Delete file/s from a folder
+  // Set directory to delete files from
+  const directory = path.join(__dirname, '../uploads/mdFiles/temp/');
+  // Read the directory
+  fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+    // Remove each file
+    for (const file of files) {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+}
+
+// Routes
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'MD to HTML Package Tester',
-    output: '<p>This will output the converted MD with the selected package</p>'
+    title: 'Marked MD to HTML Renderer',
+    output: '<p>This will show the markdown rendered in HTML</p>'
   });
 });
 
 router.get('/mdfile/upload', (req, res) => {
-  let path = __dirname + '/../public/mdFiles/README.md';
-  let file = fs.readFileSync(path, 'utf8');
-  res.send("This is a sample output: " + marked(file.toString()));
+  deleteLastUpload();
+  let samplePath = __dirname + '/../uploads/mdFiles/Sample.md';
+  let file = fs.readFileSync(samplePath, 'utf8');
+  sampleFile = marked(file.toString());
+  res.render('index', {
+    title: 'Marked MD to HTML Renderer',
+    output: sampleFile
+  });
 });
 
 router.post('/mdfile/upload', upload.single('markdown'), (req, res) => {
+  deleteLastUpload();
   console.log(req.file)
   let fileLoc = req.file.path;
   let fileToRender = fs.readFileSync(fileLoc);
   let fileHTMLstring = marked(fileToRender.toString());
   res.render('index', {
-    title: 'Marked NPM',
+    title: 'Marked MD to HTML Renderer',
     output: fileHTMLstring
   });
-  //res.send(marked(fileToRender.toString()));
 });
 
 router.post('/marked', function(req, res){
@@ -43,7 +67,7 @@ router.post('/marked', function(req, res){
   console.log("Converting to HTML")
   let HTMLoutput = marked(req.body.mdInput)
   res.render('index', {
-    title: 'Marked NPM',
+    title: 'Marked MD to HTML Renderer',
     output: HTMLoutput
   });
 })
